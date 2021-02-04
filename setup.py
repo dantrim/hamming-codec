1d6811c28c8be41607036f39ee10fdf3f50aee28
dantrim <dantrim1023@gmail.com>
2021-02-04 13:22:04 -0800

    bump

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class InstallChecker(install):
 
 setup(
     name="hamming_codec",
-    version="0.2.2",
+    version="0.2.3",
     description="Simple encode/decode utilities for bit-error correcting Hamming codes",
     long_description=README,
     long_description_content_type="text/markdown",
