1f2bb8de0463f239c24cece8166ed974612c3ac3
dantrim <dantrim1023@gmail.com>
2021-10-25 12:42:45 -0700

    move up hamming_codec.h

--- src/cpp/hamming_codec.h
+++ src/cpp/hamming_codec.h
@@ -1,13 +1,53 @@
+/*
+ _                               _                                 _           
+| |                             (_)                               | |          
+| |__   __ _ _ __ ___  _ __ ___  _ _ __   __ _ ______ ___ ___   __| | ___  ___ 
+| '_ \ / _` | '_ ` _ \| '_ ` _ \| | '_ \ / _` |______/ __/ _ \ / _` |/ _ \/ __|
+| | | | (_| | | | | | | | | | | | | | | | (_| |     | (_| (_) | (_| |  __/ (__ 
+|_| |_|\__,_|_| |_| |_|_| |_| |_|_|_| |_|\__, |      \___\___/ \__,_|\___|\___|
+                                          __/ |                                
+                                         |___/                                  
+
+hamming-codec
+version 0.1.0
+https://github.com/dantrim/hamming-codec
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+SPDX-License-Identifier: MIT
+Copyright (c) 2020 Daniel Antrim
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef INCLUDE_HAMMING_CODEC_H
+#define INCLUDE_HAMMING_CODEC_H
+
 #include <string>
 #include <vector>
 #include <sstream>
-#include <algorithm>
 #include <numeric> // accumulate
 #include <math.h>
 
-#include <iomanip>
 namespace hamming_codec {
 
+	// enumeration of the supported placements
+	// of the computed parity bits in encoded messages
     enum class ParityLocation {
         DEFAULT
        ,MSB
@@ -44,8 +84,6 @@ namespace hamming_codec {
         template std::string join<int>(const std::vector<int>&);
 
         std::vector<char> split(const std::string& s) {
-            //std::vector<char> out(s.length());
-            //std::copy(s.begin(), s.end(), out.begin());
             std::vector<char> out(s.begin(), s.end());
             return out;
         }
@@ -341,3 +379,5 @@ namespace hamming_codec {
     }
 
 } // namespace hamming_codec
+
+#endif // INCLUDE_HAMMING_CODEC_H
