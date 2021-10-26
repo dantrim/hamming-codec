6ec18e9828dccc0d8cb4a3beb9d903e61da81f55
dantrim <dantrim1023@gmail.com>
2021-10-26 11:06:30 -0700

    Bump version: 0.2.0 → 0.3.0

--- src/cpp/hamming_codec.h
+++ src/cpp/hamming_codec.h
@@ -9,7 +9,7 @@
                                          |___/                                  
 
 hamming-codec
-version 0.2.0
+version 0.3.0
 https://github.com/dantrim/hamming-codec
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
